e80794d4c4cdc7e6f81666dc9f68ae374dd752c9
Gabriel Ikeda <gabrielikeda55@gmail.com>
2017-05-13 17:02:16 -0300

    Inserir e alterar

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 require_once("config.php");
 
 //Carrega um usuario
-// $jose = new Usuario();
-// $jose->loadById(3);
+ // $jose = new Usuario();
+ // $jose->loadById(3);
 //
 // echo $jose;
 
@@ -19,9 +19,24 @@ require_once("config.php");
 // echo json_encode($search);
 
 //Carrega um usuário usando o login e a setDessenha
+// $usuario = new Usuario();
+// $usuario->login("jose","1235678");
+// //
+// // echo $usuario;
+//
+
+//Criando um novo usuário
+// $aluno = new Usuario("hehe","lilolu");
+//
+// $aluno->insert();
+//
+// echo $aluno;
+
 $usuario = new Usuario();
-$usuario->login("jose","1235678");
+
+$usuario->loadById(5);
+
+$usuario->update("professor", "mudarsenha");
 
 echo $usuario;
-
  ?>
